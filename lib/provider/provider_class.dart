3224c11bbb4a6dc0e5621fb24aff85009e52f6a5
vladStar223 <73715696+vladStar223@users.noreply.github.com>
2023-07-28 13:27:02 +0300

    update_11.0.7.2

--- lib/provider/provider_class.dart
+++ lib/provider/provider_class.dart
@@ -671,30 +671,3 @@ class Change_activ_screen_out_input extends ChangeNotifier {
     notifyListeners();
   } // переключение на нужный экран результата
 } // отвечает за   переключение  активных окнов ввода и вывода
-
-class Change_of_function extends ChangeNotifier {
-  // класс отвечает за  измения функционало приложение
-  bool calculator = true;
-  bool calculator_expanded = false;
-  bool equation_function = false;
-  void change_state_calculator() {
-    calculator = true;
-    calculator_expanded = false;
-    equation_function = false;
-    notifyListeners();
-  } // функция запуска калькулятора
-
-  void change_state_calculator_expanded() {
-    calculator = true;
-    calculator_expanded = true;
-    equation_function = false;
-    notifyListeners();
-  } // функция запуска увеличенного калькулятора
-
-  void change_state_equation_function() {
-    calculator = false;
-    calculator_expanded = false;
-    equation_function = true;
-    notifyListeners();
-  } // функция запуска  решение уравний
-}
