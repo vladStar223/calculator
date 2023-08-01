886bace3ee2ebf31866b2d4f3e7cab11b972c01a
vladStar223 <73715696+vladStar223@users.noreply.github.com>
2023-08-01 22:00:44 +0300

    update_11.0.8.2

--- lib/provider/calcString.dart
+++ lib/provider/calcString.dart
@@ -69,8 +69,6 @@ dynamic sin_1(dynamic a) {
   var n = a;
   var t1;
   if (rad_deg = true) {
-    var angle = Angle.fromDegrees(n);
-    t1 = angle.sin();
     if (n == 0) {
       t1 = sin0;
     } else if (n == 30) {
@@ -81,9 +79,56 @@ dynamic sin_1(dynamic a) {
       t1 = sin90;
     } else if (n == 180) {
       t1 = 0;
+    } else {
+      var angle = Angle.fromDegrees(n);
+      t1 = angle.sin();
     }
   } else {
     t1 = sin(a);
+  }
+  return t1;
+}
+
+dynamic cos_1(dynamic a) {
+  var n = a;
+  var t1;
+  if (rad_deg = true) {
+    if (n == 0) {
+      t1 = cos0;
+    } else if (n == 30) {
+      t1 = cos30;
+    } else if (n == 45) {
+      t1 = cos45;
+    } else if (n == 90) {
+      t1 = cos90;
+    } else if (n == 180) {
+      t1 = -1;
+    } else {
+      var angle = Angle.fromDegrees(n);
+      t1 = angle.cos();
+    }
+  } else {
+    t1 = cos(a);
+  }
+  return t1;
+}
+
+dynamic tan_1(dynamic a) {
+  var n = a;
+  var t1;
+  if (rad_deg = true) {
+    if (n == 0) {
+      t1 = tan0;
+    } else if (n == 30) {
+      t1 = tan30;
+    } else if (n == 60) {
+      t1 = tan60;
+    } else {
+      var angle = Angle.fromDegrees(n);
+      t1 = angle.tan();
+    }
+  } else {
+    t1 = tan(a);
   }
   return t1;
 }
