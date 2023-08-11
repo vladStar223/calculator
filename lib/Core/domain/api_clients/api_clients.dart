027c8922334b3834fffe54d582395f4b80acffd9
vladStar223 <73715696+vladStar223@users.noreply.github.com>
2023-08-11 20:13:07 +0300

    update_11.0.3.6.7

--- lib/Core/domain/api_clients/api_clients.dart
+++ lib/Core/domain/api_clients/api_clients.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 class ApiClient {
   final client = HttpClient();
   void getPost() async {
-    final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    final Uri url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
     final request = await client.getUrl(url);
     final response = await request.close();
     final jsonStrings = await response.transform(utf8.decoder).toList();
