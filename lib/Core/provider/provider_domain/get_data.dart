fc77ccc684ca6be310b1b28e0d7bd7b6d2cd3340
vladStar223 <73715696+vladStar223@users.noreply.github.com>
2023-08-13 17:11:33 +0300

    update_11.0.3.6.9.7.1

--- lib/Core/provider/provider_domain/get_data.dart
+++ lib/Core/provider/provider_domain/get_data.dart
@@ -1,18 +1,31 @@
+import 'package:calculator/Core/domain/api_clients/api_clients.dart';
 import 'package:calculator/Core/domain/entity/post.dart';
 import 'package:flutter/widgets.dart';
 
-import '../../domain/entity/bot.dart';
+import '../../domain/entity/valute.dart';
 
 class Get_data extends ChangeNotifier {
   bool get_data = true;
   String state_data = "Состояние";
-  var _valutes = <Valute>[];
-  void get_valutes() {
-    get_data = true;
-  }
+  var valutes = <Valute>[];
+  var name_code = [];
+  var x = ApiClient();
+  List<Valute> get valute => List.unmodifiable(valutes);
+  Future<void> get_post() async {
+    final valuteResponse = await x.fetchPost();
+    if (valuteResponse == "Данные не получены") {
+      state_data = "ошибка";
+    } else {
+      valutes.addAll((valuteResponse.valute.values as Iterable<Valute>)
+          .toList(growable: false));
+      name_code.addAll((valuteResponse.valute.keys as Iterable<String>)
+          .toList(growable: false));
+      get_data = false;
+      state_data = "Данные получены";
+    }
 
-  void set_valutes(var x) {
-    _valutes = x;
+    //print(name_code[0]);
+    //print(valutes[0].Name);
     notifyListeners();
   }
 }
--- lib/screens/Currency_converter/converter.dart
+++ lib/screens/Currency_converter/converter.dart
@@ -2,10 +2,15 @@ import 'dart:convert';
 
 import 'package:calculator/Core/domain/entity/post.dart';
 import 'package:calculator/Core/domain/entity/valute.dart';
+import 'package:calculator/Core/provider/provider_domain/get_data.dart';
+import 'package:calculator/Screens/keyboard.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 
 import '../../Core/domain/api_clients/api_clients.dart';
+import '../../theme/color/theme.dart';
 
 class Currency_conver extends StatefulWidget {
   @override
@@ -16,20 +21,201 @@ class _Currency_converState extends State<Currency_conver> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build\
-    var _future;
-    var x;
-    x = ApiClient();
-    //_future = x.fetchPost();
+    var AppColors = Provider.of<AppColor>(context);
+    var data = Provider.of<Get_data>(context);
     return Scaffold(
+      backgroundColor: AppColors.fon,
       body: Column(
         children: [
-          Row(),
+          SizedBox(
+            height: 1.h,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 4.h,
+                width: 100.w,
+                child: ListTile(
+                  title: Text(
+                    textAlign: TextAlign.center,
+                    data.state_data,
+                    style: TextStyle(
+                        color: AppColors.white,
+                        fontSize: 5.w,
+                        fontWeight: FontWeight.w100,
+                        fontFamily: "Nokora"),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {
+                      if (data.get_data == true) {
+                        data.get_post();
+                      } else {
+                        print("данные получены");
+                      }
+                    },
+                    tooltip: 'Обновить валюты',
+                    splashRadius: 30,
+                    splashColor: Colors.black12,
+                    style: IconButton.styleFrom(
+                        animationDuration: const Duration(seconds: 100)),
+                    icon: Icon(
+                      Icons.browser_updated,
+                      color: AppColors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 5.h,
+          ),
+          Input_out_valute(),
+          SizedBox(
+            height: 10.h,
+          ),
         ],
       ),
     );
   }
 }
+
+class Input_out_valute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    var AppColors = Provider.of<AppColor>(context);
+    return Column(children: [
+      Container(
+        height: 6.h,
+        width: 95.w,
+        decoration: BoxDecoration(
+          color: AppColors.output,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ListTile(
+          title: Text(
+            textAlign: TextAlign.start,
+            "USA",
+            style: TextStyle(
+                color: AppColors.textcolor2,
+                fontSize: 8.w,
+                fontWeight: FontWeight.w100,
+                fontFamily: "Nokora"),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            tooltip: 'Обновить валюты',
+            splashRadius: 30,
+            splashColor: Colors.black,
+            style: IconButton.styleFrom(
+                animationDuration: const Duration(seconds: 100)),
+            icon: Icon(
+              Icons.browser_updated,
+              color: AppColors.fon,
+            ),
+          ),
+        ),
+      ),
+      Container(
+        height: 6.h,
+        width: 95.w,
+        decoration: BoxDecoration(
+          color: AppColors.output,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ListTile(
+          title: Text(
+            textAlign: TextAlign.start,
+            "USA",
+            style: TextStyle(
+                color: AppColors.textcolor2,
+                fontSize: 8.w,
+                fontWeight: FontWeight.w100,
+                fontFamily: "Nokora"),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            tooltip: 'Обновить валюты',
+            splashRadius: 30,
+            splashColor: Colors.black,
+            style: IconButton.styleFrom(
+                animationDuration: const Duration(seconds: 100)),
+            icon: Icon(
+              Icons.browser_updated,
+              color: AppColors.fon,
+            ),
+          ),
+        ),
+      ),
+      Container(
+        height: 6.h,
+        width: 95.w,
+        decoration: BoxDecoration(
+          color: AppColors.output,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ListTile(
+          title: Text(
+            textAlign: TextAlign.start,
+            "USA",
+            style: TextStyle(
+                color: AppColors.textcolor2,
+                fontSize: 8.w,
+                fontWeight: FontWeight.w100,
+                fontFamily: "Nokora"),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            tooltip: 'Обновить валюты',
+            splashRadius: 30,
+            splashColor: Colors.black,
+            style: IconButton.styleFrom(
+                animationDuration: const Duration(seconds: 100)),
+            icon: Icon(
+              Icons.browser_updated,
+              color: AppColors.fon,
+            ),
+          ),
+        ),
+      ),
+      Container(
+        height: 6.h,
+        width: 95.w,
+        decoration: BoxDecoration(
+          color: AppColors.output,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ListTile(
+          title: Text(
+            textAlign: TextAlign.start,
+            "USA",
+            style: TextStyle(
+                color: AppColors.textcolor2,
+                fontSize: 8.w,
+                fontWeight: FontWeight.w100,
+                fontFamily: "Nokora"),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            tooltip: 'Обновить валюты',
+            splashRadius: 30,
+            splashColor: Colors.black,
+            style: IconButton.styleFrom(
+                animationDuration: const Duration(seconds: 100)),
+            icon: Icon(
+              Icons.browser_updated,
+              color: AppColors.fon,
+            ),
+          ),
+        ),
+      ),
+    ]);
+    throw UnimplementedError();
+  }
+}
 /*
+
  child: FutureBuilder<Post>(
           future: _future,
           builder: (context, snapshot) {
